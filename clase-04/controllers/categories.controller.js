136ac3939f51d4fbec2b4cb9301006b38a90c86e
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-04-11 13:51:40 +0200

    delete Category

--- clase-04/controllers/categories.controller.js
+++ clase-04/controllers/categories.controller.js
@@ -56,3 +56,17 @@ export const updateCategory = (req, res) => {
   res.json(category);
 }
 
+export const deleteCategory = (req, res) => {
+  const id = Number(req.params.id);
+
+  if (Number.isNaN(id)){
+    return res.status(400).json({error: 'Invalid id'});
+  } 
+  const categoryIndex = categories.findIndex((cat) => cat.id == id);
+  if (categoryIndex === -1){
+    return res.status(404).json({error: 'Category not found'});
+  } 
+  categories.splice(categoryIndex, 1);
+
+  res.status(204).send();
+}
--- clase-04/routes/categories.router.js
+++ clase-04/routes/categories.router.js
@@ -5,11 +5,12 @@ const router = Router();
 import { getAllCategories, updateCategory } from "../controllers/categories.controller.js";
 import { getCategoryById } from "../controllers/categories.controller.js";
 import { createCategory } from "../controllers/categories.controller.js";
-
+import { deleteCategory } from "../controllers/categories.controller.js";   
 
 router.get("/", getAllCategories);
 router.get("/:id", getCategoryById);
 router.post("/", createCategory);
 router.put("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
 
 export default router;
