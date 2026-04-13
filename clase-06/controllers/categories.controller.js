db355986476694d924b37a947b361900a561d20d
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-04-13 06:55:59 +0200

    Clase 06

---
+++ clase-06/controllers/categories.controller.js
@@ -0,0 +1,89 @@
+const categories = [
+  {id: 1, name: "Electrodomestic", description: "Products for home use",},
+  {id: 2, name: "Technology", description: "Products related to technology",}
+];
+
+import Category from "../models/Category.js";
+
+export const getAllCategories = async (req, res) => {
+  const categories = await Category.find();
+  res.json(categories);
+}
+
+export const getCategoryById = (req, res) => {
+  const category = categories.find((p) => p.id == req.params.id);
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)){
+    return res.status(400).json({error: 'Invalid id'});
+  }
+
+  if (!category){
+    return res.status(404).json({error: 'Category not found'});
+  }
+  res.json(category);
+}
+
+export const createCategory = (req, res) => {
+  if(!req.body.name == undefined){
+    return res.status(422).json({error: "name is required"});    
+  }
+
+  const newCategory ={
+    id: Date.now(),
+    name: req.body.name,
+    description: req.body.description,
+  }
+
+  categories.push(newCategory);
+
+  res.status(201).json(newCategory);
+}
+
+export const updateCategory = (req, res) => {
+  const id = Number(req.params.id);
+
+  if (Number.isNaN(id)){
+    return res.status(400).json({error: 'Invalid id'});
+  }
+  const category = categories.find((cat) => cat.id == id);
+  if (!category){
+    return res.status(404).json({error: 'Category not found'});
+  }
+
+  const { name, description } = req.body;
+
+  category.name = name;
+  category.description = description;
+
+  res.json(category);
+}
+
+export const deleteCategory = (req, res) => {
+  const id = Number(req.params.id);
+
+  if (Number.isNaN(id)){
+    return res.status(400).json({error: 'Invalid id'});
+  } 
+  const categoryIndex = categories.findIndex((cat) => cat.id == id);
+  if (categoryIndex === -1){
+    return res.status(404).json({error: 'Category not found'});
+  } 
+  categories.splice(categoryIndex, 1);
+
+  res.status(204).send();
+}
+
+export const searchCategory = async(req, res) => {
+  const { name } = req.query;
+
+  if (!name){
+    return res.status(400).json({error: 'Name query parameter is required'});
+  }
+
+  const categories = await Category.find({
+    name: { $regex: name, $options: "i" },
+  });
+
+  res.json(categories);
+}
---
+++ clase-06/controllers/products.controller.js
@@ -0,0 +1,112 @@
+import { validateStock, validatePrice } from "../utils/validators.js";
+
+import Product from "../models/Product.js";
+
+const products = [
+  { id: 1, name: "Product 1", price: 10.99 },
+  { id: 2, name: "Product 2", price: 15.99 },
+];
+
+export const getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+export const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+
+    if (isNaN(id)) {
+      return res.status(400).json({ error: "Invalid id" });
+    }
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({error: "Invalid product id"});
+  }
+};
+
+export const createProduct = async (req, res) => {
+  if (!validateStock(req.body.stock)) {
+    return res.status(422).json({ error: "Invalid stock" });
+  }
+
+  if (!validatePrice(req.body.price)) {
+    return res.status(422).json({ error: "Invalid price" });
+  }
+
+  const data = {
+    name: req.body.name,
+    price: Number(req.body.price),
+    stock: Number(req.body.stock),
+  };
+
+  const product = new Product(data);
+  await product.save();
+
+  res.status(201).json(product);
+};
+
+export const updateProduct = (req, res) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid id" });
+  }
+
+  const product = products.find((p) => p.id == id);
+  if (!product) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  if (!validateStock(req.body.stock)) {
+    return res.status(422).json({ error: "Invalid stock" });
+  }
+
+  if (!validatePrice(req.body.price)) {
+    return res.status(422).json({ error: "Invalid price" });
+  }
+
+  const { name, price, stock } = req.body;
+
+  product.name = name;
+  product.price = Number(price);
+  product.stock = Number(stock);
+
+  res.json(product);
+};
+
+export const deleteProduct = (req, res) => {
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid id" });
+  }
+  const productIndex = products.findIndex((p) => p.id == id);
+
+  if (productIndex === -1) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  products.splice(productIndex, 1);
+
+  res.status(204).send();
+};
+
+export const searchProduct = async (req, res) => {
+  const { name } = req.query;
+
+  if (!name) {
+    return res.status(422).json({ error: "Name query parameter is required" });
+  }
+
+  const products = await Product.find({
+    name: { $regex: name, $options: "i" },
+  });
+
+  res.json(products);
+};
