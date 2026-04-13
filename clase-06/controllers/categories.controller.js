20f65cbb26334a799bd695d4027e76c903860175
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-04-13 20:48:07 +0200

    deleteProduct & deleteCategory

--- clase-06/controllers/categories.controller.js
+++ clase-06/controllers/categories.controller.js
@@ -62,20 +62,18 @@ export const updateCategory = async (req, res) => {
   }
 };
 
-export const deleteCategory = (req, res) => {
-  const id = Number(req.params.id);
-
-  if (Number.isNaN(id)) {
-    return res.status(400).json({ error: "Invalid id" });
-  }
-  const categoryIndex = categories.findIndex((cat) => cat.id == id);
-  if (categoryIndex === -1) {
+export const deleteCategory = async (req, res) => {
+  const { id } = req.params;
+  const categoryDelete = await Category.findByIdAndDelete(id);
+  try {
+    if (!categoryDelete) {
     return res.status(404).json({ error: "Category not found" });
   }
-  categories.splice(categoryIndex, 1);
-
   res.status(204).send();
-};
+  } catch (error) {
+    res.status(404).json({ error: "Invalid category id" });
+  }  
+}
 
 export const searchCategory = async (req, res) => {
   const { name } = req.query;
--- clase-06/controllers/products.controller.js
+++ clase-06/controllers/products.controller.js
@@ -66,33 +66,17 @@ export const updateProduct = async (req, res) => {
   }
 };
 
-export const deleteProduct = (req, res) => {
-  const id = Number(req.params.id);
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
 
-  if (isNaN(id)) {
-    return res.status(400).json({ error: "Invalid id" });
-  }
-  const productIndex = products.findIndex((p) => p.id == id);
-
-  if (productIndex === -1) {
+ const productDelete = await Product.findByIdAndDelete(id);
+ try{
+    if (!productDelete) {
     return res.status(404).json({ error: "Product not found" });
   }
-
-  products.splice(productIndex, 1);
-
-  res.status(204).send();
-};
-
-export const searchProduct = async (req, res) => {
-  const { name } = req.query;
-
-  if (!name) {
-    return res.status(422).json({ error: "Name query parameter is required" });
-  }
-
-  const products = await Product.find({
-    name: { $regex: name, $options: "i" },
-  });
-
-  res.json(products);
+  
+ res.status(204).send();
+ } catch (error) {
+  res.status(404).json({ error: "Invalid product id" });
+ }
 };
