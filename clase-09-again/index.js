26040e97379811c10901f9f03f4b6a7e8fc7aab4
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-05-12 21:18:07 +0200

    Test POST Product y Category

--- clase-09-again/index.js
+++ clase-09-again/index.js
@@ -1,5 +1,3 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(3000, () => console.log(`http://localhost:3000`));
