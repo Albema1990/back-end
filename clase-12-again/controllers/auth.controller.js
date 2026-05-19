9d88f729811e7b7a6779a85027c32b1758cf2a45
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-05-19 19:44:01 +0200

    Verificacion email y password

--- clase-12-again/controllers/auth.controller.js
+++ clase-12-again/controllers/auth.controller.js
@@ -4,12 +4,30 @@ import bcrypt from "bcryptjs";
 export const register = async (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required" });
+  }
+
+  // if(!email.includes("@")) {
+  //   return res.status(400).json({ error: "Invalid email format" });
+  // }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+
+  if(!emailRegex.test(email)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
+
+  if(password.length < 5) {
+    return res.status(400).json({ error: "Password must be at least 5 characters long" });
+  }
+
   const hash = await bcrypt.hash(password, 10);
 
   const user = await User.create({
-     email,
-     password: hash,
- });
+    email,
+    password: hash,
+  });
 
   res.status(201).json({
     id: user.id,
