6629155f2f57344bffeba0f552117a68760aa5ae
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-05-19 20:02:35 +0200

    Register - Node.js - Clase 12

--- clase-12-again/controllers/auth.controller.js
+++ clase-12-again/controllers/auth.controller.js
@@ -2,35 +2,48 @@ import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ error: "Email and password are required" });
+    if (!email || !password) {
+      return res.status(400).json({ error: "Email and password are required" });
+    }
+
+    // if(!email.includes("@")) {
+    //   return res.status(400).json({ error: "Invalid email format" });
+    // }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    if (password.length < 5) {
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 5 characters long" });
+    }
+
+    const existingUser = await User.findOne({email});
+
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already in use" });
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      email,
+      password: hash,
+    });
+
+    res.status(201).json({
+      id: user.id,
+      email: user.email,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
-
-  // if(!email.includes("@")) {
-  //   return res.status(400).json({ error: "Invalid email format" });
-  // }
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-
-  if(!emailRegex.test(email)) {
-    return res.status(400).json({ error: "Invalid email format" });
-  }
-
-  if(password.length < 5) {
-    return res.status(400).json({ error: "Password must be at least 5 characters long" });
-  }
-
-  const hash = await bcrypt.hash(password, 10);
-
-  const user = await User.create({
-    email,
-    password: hash,
-  });
-
-  res.status(201).json({
-    id: user.id,
-    email: user.email,
-  });
 };
